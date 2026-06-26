2e3a65f28fba385c9a78826525a3b2616ca69fe2
bzfriend1217-lgtm <bzfriend1217@gmail.com>
2026-06-26 17:11:37 +0900

    9

---
+++ src/main/java/org/example/Classroom.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import java.util.*;
+
+public class Classroom {
+    private final List<Student> students = new ArrayList<>();
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public List<Student> getStudents() {
+
+        return new ArrayList<>(this.students);
+    }
+
+    public Optional<Student> findById(int id) {
+
+        /*
+        Optional<Student> studentOptional = Optional.empty();
+
+        for (Student student : students) {
+            if (student.getId() == id) {
+                studentOptional = Optional.of(student);
+                break;
+            }
+        }
+
+        return studentOptional;
+        */
+
+        return students.stream()
+                .filter(student -> student.getId() == id)
+                .findFirst();
+    }
+
+
+    public List<Student> getPassedStudents() {
+        return students.stream()
+                .filter(student -> student.getScore() >= 60)
+                .toList();
+    }
+
+    public Optional<Student> findTopStudent() {
+        return students.stream()
+                .max(Comparator.comparingInt(Student::getScore));
+    }
+
+    public List<Student> searchByName(String name) {
+        return students.stream()
+                .filter(student -> (student.getName()).contains(name))
+                .toList();
+    }
+}
+
